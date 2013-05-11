b5aa6384f74ab51f16a9feee5e54c8a95761dac6
Mike McHenry <michaelomchenry@gmail.com>
2013-05-11 09:27:57 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tooler::Application.config.secret_token = 'fee0e883c1a7bbf1153c694003099a147ec3c99d7e7f7be898cb0a613213d9cf7736ad94b7c72bd9b654190bcdd7f9bb2a55ac2ddcc04372528a624e224737d6'
