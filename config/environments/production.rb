4641fb4512aba70202dffe64c40a0e408b53d089
Mike McHenry <michaelomchenry@gmail.com>
2013-05-13 20:53:05 -0400

    devise environment mods

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Tooler::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # DONT FORGET TO CHANGE THIS
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
