2db2948b2098cf8d1715de53fc6bb86f863084ce
Mike McHenry <michaelomchenry@gmail.com>
2013-05-16 13:23:46 -0400

    more relationship building

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   attr_accessible :assettag, :description
+
+  has_many :transactions, foreign_key: 'item_id'
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :employee_id, :item_id, :status
+
+  belongs_to :item
+
+  delegate :phone, :name, to: :employee
+  delegate :description, :assettag, to: :item
 end
