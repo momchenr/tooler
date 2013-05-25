87dc9591886714d1a2a60ae59351d9db2dfb6ceb
Mike McHenry <michaelomchenry@gmail.com>
2013-05-25 15:45:37 -0400

    suspected param case sensitivity issue

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,7 +7,7 @@ class TwilioController < ApplicationController
     end
   
   def create
-    phone_number = params[:from][/\d+/]
+    phone_number = params[:From][/\d+/]
     employee = Employee.where(phone: phone_number).first
     if employee
       transaction = Transaction.new(:item_id => [:Body], :employee_id => employee, :status => true)
