174fc29d0d3a3032f01e115e5d7dc6e74405d4f8
Mike McHenry <michaelomchenry@gmail.com>
2013-06-06 16:19:01 -0400

    remove first method

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,7 +8,7 @@ class TwilioController < ApplicationController
   
   def create
     phone_number = params[:From][/\d+/]
-    employee = Employee.where(phone: phone_number).first
+    employee = Employee.where(phone: phone_number)
     if employee
       @transaction = Transaction.new(:item_id => params[:Body], :employee_id => employee, :status => true)
       @transaction.save
