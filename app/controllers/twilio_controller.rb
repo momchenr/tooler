43f914732ae6aee38916893785db032fc8b5ddec
Mike McHenry <michaelomchenry@gmail.com>
2013-06-06 12:47:25 -0400

    further testing of controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,12 +7,8 @@ class TwilioController < ApplicationController
     end
   
   def create
-    phone_number = params[:From][/\d+/]
-    employee = Employee.where(phone: phone_number).first
-    if employee
-      transaction = Transaction.new(:item => [:Body], :employee => 1, :status => true)
-      @transaction.save
-    end
+    @transaction = Transaction.new(:item => [:Body], :employee => 1, :status => true)
+    @transaction.save
   end
 
 end
