eacc1afbd9bd8f795bf2d85a2274cdb8f5bae17f
Mike McHenry <michaelomchenry@gmail.com>
2013-06-06 20:47:51 -0400

    problem passing ids

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,7 +10,7 @@ class TwilioController < ApplicationController
     phone_number = params[:From][/\d+/].to_s
     employee = Employee.where(phone: phone_number.to_s).first
     if employee
-      @transaction = Transaction.new(:item_id => params[:Body], :employee_id => employee, :status => true)
+      @transaction = Transaction.new(:item_id => params[:Body], :employee_id => employee[:id], :status => true)
       @transaction.save
     end
   end
