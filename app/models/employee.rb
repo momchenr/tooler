95de9285b1db0ea114e66840b20b908d37f2e73e
Mike McHenry <michaelomchenry@gmail.com>
2013-06-14 14:48:57 -0400

    has_many through assoc

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,6 @@
 class Employee < ActiveRecord::Base
   attr_accessible :name, :phone
+  
   has_many :transactions
+  has_many :items, :through => :transactions
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ActiveRecord::Base
   attr_accessible :assettag, :description
 
   has_many :transactions
+  has_many :employees, :through => :transactions
 end
