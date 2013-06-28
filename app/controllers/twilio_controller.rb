01d8d6617f148dcc1cb84792ed0fa40f2f85892e
Mike McHenry <michaelomchenry@gmail.com>
2013-06-28 13:03:07 -0400

    check in priorly checkedout items

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -21,7 +21,15 @@ class TwilioController < ApplicationController
     employee = Employee.where(phone: phone_number.to_s).first
     item = Item.where(assettag: item_asset.to_s).first.upcase
 
-    if employee
+    if employee && !item.last_transaction.status
+      # generate checkin for last employee that had it
+      @transaction = Transaction.new(:item_id => item[:id], :employee_id => item.last_transaction.employee[:id], :status => true)
+      @transaction.save
+      # generate checkout for incoming twilio message
+      @transaction = Transaction.new(:item_id => item[:id], :employee_id => employee[:id], :status => item_status)
+      @transaction.save
+    elsif employee && item.last_transaction.status
+      # if item is currently checkedin, just make a new checkout
       @transaction = Transaction.new(:item_id => item[:id], :employee_id => employee[:id], :status => item_status)
       @transaction.save
     end
